daa729a97198265f81a67ac8d6585e9b04ffaa04
jlcuellar2014 <35740606+jlcuellar2014@users.noreply.github.com>
2023-11-30 18:33:20 +0100

    The endpoint was made to request all the Devices and their connected Switches

---
+++ SwitchTester/SwitchTesterApi/Controllers/DevicesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SwitchTesterApi.Services;
+
+namespace SwitchTesterApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevicesController : ControllerBase
+    {
+        private readonly IDevicesService service;
+
+        public DevicesController(IDevicesService service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync() {
+            try
+            {
+                var results = await service.GetDeviceConnectedAsync();
+                return Ok(results);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
