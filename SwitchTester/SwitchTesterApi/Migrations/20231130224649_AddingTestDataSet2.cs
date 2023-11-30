4b4535d5cfdeecf43c6f00eeaefd951e9fb36939
jlcuellar2014 <35740606+jlcuellar2014@users.noreply.github.com>
2023-11-30 23:51:17 +0100

    More test dataset

---
+++ SwitchTester/SwitchTesterApi/Migrations/20231130224649_AddingTestDataSet2.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SwitchTesterApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingTestDataSet2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+               table: "DevicePorts",
+               columns: new[] { "DeviceId", "Port" },
+               values: new object[,] {
+                    {1, 6101},
+                    {1, 6102},
+                    {2, 6201},
+                    {2, 6202},
+               }
+           );
+
+           migrationBuilder.InsertData(
+               table: "SwitchPorts",
+               columns: new[] { "SwitchId", "Port" },
+               values: new object[,] {
+                    {1, 6101},
+                    {1, 6102},
+                    {2, 6201},
+                    {2, 6202},
+               }
+           );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
