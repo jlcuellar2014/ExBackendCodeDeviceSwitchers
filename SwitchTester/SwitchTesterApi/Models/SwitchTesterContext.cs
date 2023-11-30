46d88eee8e446f4b9ebb8246fbf42f97d62c2dfd
jlcuellar2014 <35740606+jlcuellar2014@users.noreply.github.com>
2023-11-30 17:13:59 +0100

    Teh database Context for the application was made

---
+++ SwitchTester/SwitchTesterApi/Models/SwitchTesterContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SwitchTesterApi.Models
+{
+    public class SwitchTesterContext: DbContext
+    {
+        public DbSet<Switch> Switches { get; set; }
+        public DbSet<Device> Devices { get; set; }
+        public DbSet<SwitchPort> SwitchPorts { get; set; }
+        public DbSet<DevicePort> DevicePorts { get; set; }
+        public DbSet<DeviceSwitchConnection> DeviceSwitchConnections { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //  I'm sure that is not the way to do it :)
+            optionsBuilder.UseSqlite($"Data Source=SwitchTester.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DeviceSwitchConnection>()
+               .HasKey(o => new { o.DeviceId, o.SwitchId, o.Port });
+
+            modelBuilder.Entity<DevicePort>()
+                .HasKey(o => new { o.DeviceId, o.Port });
+            
+            modelBuilder.Entity<SwitchPort>()
+                .HasKey(o => new { o.SwitchId, o.Port });
+        }
+    }
+}
