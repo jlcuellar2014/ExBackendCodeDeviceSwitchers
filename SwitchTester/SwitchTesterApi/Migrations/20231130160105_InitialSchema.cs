aeeee398ff759c85c44a44177f6e9e88ec7eacf5
jlcuellar2014 <35740606+jlcuellar2014@users.noreply.github.com>
2023-11-30 17:15:17 +0100

    Migrations for initial schema and a test dataset were made

---
+++ SwitchTester/SwitchTesterApi/Migrations/20231130160105_InitialSchema.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SwitchTesterApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    DeviceId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    HostName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.DeviceId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Switches",
+                columns: table => new
+                {
+                    SwitchId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    HostName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Switches", x => x.SwitchId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DevicePorts",
+                columns: table => new
+                {
+                    DeviceId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Port = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DevicePorts", x => new { x.DeviceId, x.Port });
+                    table.ForeignKey(
+                        name: "FK_DevicePorts_Devices_DeviceId",
+                        column: x => x.DeviceId,
+                        principalTable: "Devices",
+                        principalColumn: "DeviceId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DeviceSwitchConnections",
+                columns: table => new
+                {
+                    SwitchId = table.Column<int>(type: "INTEGER", nullable: false),
+                    DeviceId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Port = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeviceSwitchConnections", x => new { x.DeviceId, x.SwitchId, x.Port });
+                    table.ForeignKey(
+                        name: "FK_DeviceSwitchConnections_Devices_DeviceId",
+                        column: x => x.DeviceId,
+                        principalTable: "Devices",
+                        principalColumn: "DeviceId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DeviceSwitchConnections_Switches_SwitchId",
+                        column: x => x.SwitchId,
+                        principalTable: "Switches",
+                        principalColumn: "SwitchId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SwitchPorts",
+                columns: table => new
+                {
+                    SwitchId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Port = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SwitchPorts", x => new { x.SwitchId, x.Port });
+                    table.ForeignKey(
+                        name: "FK_SwitchPorts_Switches_SwitchId",
+                        column: x => x.SwitchId,
+                        principalTable: "Switches",
+                        principalColumn: "SwitchId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeviceSwitchConnections_SwitchId",
+                table: "DeviceSwitchConnections",
+                column: "SwitchId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DevicePorts");
+
+            migrationBuilder.DropTable(
+                name: "DeviceSwitchConnections");
+
+            migrationBuilder.DropTable(
+                name: "SwitchPorts");
+
+            migrationBuilder.DropTable(
+                name: "Devices");
+
+            migrationBuilder.DropTable(
+                name: "Switches");
+        }
+    }
+}
---
+++ SwitchTester/SwitchTesterApi/Migrations/20231130160329_AddingTestDataSet.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using SwitchTesterApi.Models;
+
+#nullable disable
+
+namespace SwitchTesterApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingTestDataSet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Devices",
+                columns: new[] { "DeviceId", "HostName" },
+                values: new object[,] {
+                    {1, "Device 1"},
+                    {2, "Device 2"},
+                }
+            );
+
+            migrationBuilder.InsertData(
+                table: "DevicePorts",
+                columns: new[] { "DeviceId", "Port" },
+                values: new object[,] {
+                    {1, 5101},
+                    {1, 5102},
+                    {2, 5201},
+                    {2, 5202},
+                }
+            );
+
+            migrationBuilder.InsertData(
+                table: "Switches",
+                columns: new[] { "SwitchId", "HostName" },
+                values: new object[,] {
+                    {1, "Switch 1"},
+                    {2, "Switch 2"},
+                }
+            );
+
+            migrationBuilder.InsertData(
+                table: "SwitchPorts",
+                columns: new[] { "SwitchId", "Port" },
+                values: new object[,] {
+                    {1, 5101},
+                    {1, 5102},
+                    {2, 5201},
+                    {2, 5202},
+                }
+            );
+
+            migrationBuilder.InsertData(
+                table: "DeviceSwitchConnections",
+                columns: new[] { "SwitchId", "DeviceId","Port" },
+                values: new object[,] {
+                    {1, 1, 5101},
+                    {1, 1, 5102},
+                    {2, 2, 5201},
+                    {2, 2, 5202},
+                }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
